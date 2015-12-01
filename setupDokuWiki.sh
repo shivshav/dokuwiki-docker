580d92d23862be49a7fb1f193498d024248d3e86
shiv <sprasad0603@gmail.com>
2015-11-30 23:29:13 -0800

    Made restart parameterized and trying to consolidate commands in the setupDokuwiki script to make it more maintainable

--- setupDokuWiki.sh
+++ setupDokuWiki.sh
@@ -13,28 +13,19 @@ sed -e "s/{DOKUWIKI_URI}/${DOKUWIKI_NAME}/g" ${BASEDIR}/${LOCAL_PROTECTED_PHP}.t
 sed -i "s/{LDAP_SERVER}/${LDAP_SERVER}/g" ${BASEDIR}/${LOCAL_PROTECTED_PHP}
 sed -i "s/{LDAP_ACCOUNTBASE}/${LDAP_ACCOUNTBASE}/g" ${BASEDIR}/${LOCAL_PROTECTED_PHP}
 
-docker cp ${BASEDIR}/${LOCAL_PROTECTED_PHP} ${DOKUWIKI_NAME}:/var/www/conf/${LOCAL_PROTECTED_PHP}
-docker cp ${BASEDIR}/users.auth.php ${DOKUWIKI_NAME}:/var/www/conf/users.auth.php
-docker cp ${BASEDIR}/local.php ${DOKUWIKI_NAME}:/var/www/conf/local.php
+CONF_DIR=/var/dokuwiki-storage/conf # This is where volume-backed data is stored
+docker cp ${BASEDIR}/${LOCAL_PROTECTED_PHP} ${DOKUWIKI_NAME}:${CONF_DIR}/${LOCAL_PROTECTED_PHP}
+docker cp ${BASEDIR}/users.auth.php ${DOKUWIKI_NAME}:${CONF_DIR}/users.auth.php
+docker cp ${BASEDIR}/local.php ${DOKUWIKI_NAME}:${CONF_DIR}/local.php
+docker cp ${BASEDIR}/plugins.local.php ${DOKUWIKI_NAME}:${CONF_DIR}/plugins.local.php
+docker cp ${BASEDIR}/acl.auth.php ${DOKUWIKI_NAME}:${CONF_DIR}/acl.auth.php
 
-docker cp ${BASEDIR}/plugins.local.php ${DOKUWIKI_NAME}:/var/www/conf/plugins.local.php
-docker cp ${BASEDIR}/acl.auth.php ${DOKUWIKI_NAME}:/var/www/conf/acl.auth.php
+docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data ${CONF_DIR}/*.php
 
-docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data /var/www/conf/local.php
-docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data /var/www/conf/users.auth.php
-docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data /var/www/conf/local.protected.php
-docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data /var/www/conf/plugins.local.php
-docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data /var/www/conf/acl.auth.php
+#docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data ${CONF_DIR}/local.php
+#docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data ${CONF_DIR}/users.auth.php
+#docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data ${CONF_DIR}/local.protected.php
+#docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data ${CONF_DIR}/plugins.local.php
+#docker exec ${DOKUWIKI_NAME} chown -R www-data:www-data ${CONF_DIR}/acl.auth.php
 
-docker restart wiki
-#chown -R www-data:www-data ${BASEDIR}/local.php
-#chown -R www-data:www-data ${BASEDIR}/users.auth.php
-#chown -R www-data:www-data ${BASEDIR}/local.protected.php
-#chown -R www-data:www-data ${BASEDIR}/plugins.local.php
-#chown -R www-data:www-data ${BASEDIR}/acl.auth.php
-
-
-
-
-
-
+docker restart ${DOKUWIKI_NAME}
