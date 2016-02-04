5217151422bf4d109d0312ac35908204edc90578
shiv <sprasad0603@gmail.com>
2016-02-04 00:35:55 -0800

    Removing the destroy errors

--- destroyDokuWiki.sh
+++ destroyDokuWiki.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DOKUWIKI_NAME=${DOKUWIKI_NAME:-dokuwiki}
 DOKUWIKI_VOLUME=${DOKUWIKI_VOLUME:-dokuwiki-volume}
-docker stop ${DOKUWIKI_NAME}
-docker rm -v ${DOKUWIKI_NAME}
-docker rm -v ${DOKUWIKI_VOLUME}
+
+echo "Removing ${DOKUWIKI_NAME}..."
+docker stop ${DOKUWIKI_NAME} &> /dev/null
+docker rm -v ${DOKUWIKI_NAME} &> /dev/null
+echo "Removing ${DOKUWIKI_VOLUME}..."
+docker rm -v ${DOKUWIKI_VOLUME} &> /dev/null
